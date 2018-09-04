ab27d5dc728056fa093dfea44aeb2891398a3fc6
Tolik <dzhudzhiev@gmail.com>
2018-09-04 18:17:31 +0300

    Первое дз

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Градусы");
+let Tc = 30, Tf = Tc * (9/5);
+console.log(`${Tc} градуса(ов) по Цельсию - это ${Tf} градуса(ов) по Фаренгейту`);
+
+console.log("Переменные");
+let name = "Василий", admin = name;
+console.log(`Админа зовут ${admin}`);
+
+let st = 1000 + "108";
+console.log("JS-выражение 1000 + \"108\"");
+console.log(`даст строку ${st}`);
