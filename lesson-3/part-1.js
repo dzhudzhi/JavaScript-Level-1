033313799093f93709b044eb4c5cdc3fec8b1ebe
Tolik <dzhudzhiev@gmail.com>
2018-09-13 14:18:27 +0300

    Задание №3. Простые числа

--- lesson-3/part-1.js
+++ lesson-3/part-1.js
@@ -1,8 +1,10 @@
 let i = 2;
 while (i <= 100) {
-    console.log(i++);
-    for (j = 2; j <= i; j++) {
+    let j = 2;
+    while (j <= i) {
         if (i == j) console.log(i)
-        if ((i % j) == 0) break;
+        else if ((i % j) == 0) break;
+        j++;
     }
+    i++;
 }
