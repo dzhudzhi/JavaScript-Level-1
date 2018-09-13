df6193f358ce46fbb2be7dd52a2c0e95656c4351
Tolik <dzhudzhiev@gmail.com>
2018-09-13 14:16:14 +0300

    Поправил простые числа

--- lesson-3/part-1.js
+++ lesson-3/part-1.js
@@ -1,4 +1,8 @@
-let i = 0;
+let i = 2;
 while (i <= 100) {
     console.log(i++);
+    for (j = 2; j <= i; j++) {
+        if (i == j) console.log(i)
+        if ((i % j) == 0) break;
+    }
 }
--- lesson-3/script.js
+++ lesson-3/script.js
@@ -1,6 +1,10 @@
-let i = 0, str = "*";
-while (i < 20) {
-    console.log(str);
-    str += "*";
+let i = 2;
+while (i <= 100) {
+    let j = 2;
+    while (j <= i) {
+        if (i == j) console.log(i)
+        else if ((i % j) == 0) break;
+        j++;
+    }
     i++;
 }
